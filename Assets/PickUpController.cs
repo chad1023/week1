caa56f815d74453cf21e4e8b0963cac6ee9d2457
MingWei <lab430@LAB430deMacBook-Pro.local>
2017-03-07 16:58:56 +0800

    UnityGit

---
+++ Assets/PickUpController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour {
+	public ScoreManager scoremanager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3 (0, 45, 0);
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		scoremanager.AddScore ();
+		gameObject.SetActive (false);
+	
+	}
+}
---
+++ Assets/UFOController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOController : MonoBehaviour {
+	public float speed;
+	public float forcevalue;
+	Rigidbody2D rigidbody2D=null;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 force = Vector2.zero;
+		if (Input.GetKey (KeyCode.W)) {
+			force.y += forcevalue;
+//			transform.position += new Vector3 (0, speed*Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			force.y -= forcevalue;
+//			transform.position -= new Vector3 (0, speed*Time.deltaTime, 0);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			force.x -= forcevalue;
+//			transform.position -= new Vector3 (speed*Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			force.x += forcevalue;
+//			transform.position += new Vector3 (speed*Time.deltaTime, 0, 0);
+		}
+		rigidbody2D.AddForce (force);
+		
+	}
+}
